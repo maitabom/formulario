a61e646b4436039ad62056caddd455db63493aa8
Fábio Valentim <maitabom@gmail.com>
2019-03-12 15:42:28 -0300

    Adição da funcionalidade de formulário de pergunta se deseja dizer o retorno

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $(function () {
         time_24hr: true
     });
 
-    $("#data_inicio").change(function() {
+    $("#data_inicio").change(function () {
         flatpickr("#data_termino", {
             minDate: atualizarDataInicial(),
             locale: "pt",
@@ -25,25 +25,48 @@ $(function () {
         });
     });
 
-    $("#calcular").click(function(event){
+    $("#tipo_locacao").change(function () {
+        $("#destino").show();
+        var questionaveis = [1, 2, 5, 11, 12, 14, 15, 16];
+        var tipo = eval($(this).val());
+
+        if (questionaveis.includes(tipo)) {
+            $("#questao").modal({
+                showClose: false,
+                fadeDuration: 300
+            });
+        }
+    });
+
+    $("#calcular").click(function (event) {
         event.preventDefault();
-        
-        $(".modal").modal({
+
+        $("#enviando").modal({
             showClose: false,
             fadeDuration: 900
         });
-        
-        setTimeout(function(){
+
+        setTimeout(function () {
             $(".form-inline").submit();
-        }, 2500);        
+        }, 2500);
+    });
+
+    $("#sim").click(function () {
+        $.modal.close();
+        $("#final").show();
+    });
+
+    $("#nao").click(function () {
+        $.modal.close();
+        $("#final").hide();
     });
 });
 
 function atualizarDataInicial() {
     var sdata = $("#data_inicio").val();
 
-    if(sdata == "") return "today";
-    
+    if (sdata == "") return "today";
+
     var gate = sdata.split(" ");
     var adata = gate[0].split("/");
 
